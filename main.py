2deea614201fe663c7188e2298711cbb48c4ec82
Bruno Maruwka <339804@uwr.edu.pl>
2023-04-17 22:19:13 +0200

    17_04

--- main.py
+++ main.py
@@ -96,6 +96,25 @@ class Player(pygame.sprite.Sprite):
     def draw(self, win):
         win.blit(self.sprite, (self.rect.x, self.rect.y))
 
+class Object(pygame.sprite.Sprite):
+    def __init__(self, x, y, width, height, name = None):
+        super().__init__()
+        self.react = pygame.Rect(x, y, width, height)
+        self.image = pygame.Surface((width, height),pygame.SRCALPHA)
+        self.width = width
+        self.height = height
+        self.name = name
+    def draw(self, win):
+        win.bilt(self.image, (self.rect.x, self.rect.y))
+
+class Block(Object):
+    def __init__(self, x, y, size):
+        super().__init__(x, y, size, size)
+        block = load_block(size)
+        self.image.blit(block, (0,0))
+        self.mask = pygame.mask.from_surface(self.image)
+
+
 def get_background(name):
     image = pygame.image.load(os.path.join('miejsca',name))
     background = pygame.transform.scale(image, (WIDTH, HEIGHT))
